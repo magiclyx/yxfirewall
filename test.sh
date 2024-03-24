0a8b1642b34583c9a67815263d457a829d10ff38
Yuxi Liu <liu.yuxi.canaan@gmail.com>
2024-03-25 03:09:13 +0800

    常用命令

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-sudo ./firewall.sh nat --lan enp0s8 --wan enp0s3
+sudo ./firewall start 
+sudo ./firewall clear
+sudo ./firewall default DROP --all
+#sudo ./firewall nat --snat --lan enp0s8 --wan enp0s3
+sudo ./firewall nat --snat --from-inter enp0s8 --to-inter enp0s3
+sudo ./firewall save
 
 sudo iptables -nvL
 sudo iptables -t nat -nvL
+
+#TODO
+# log
+# DEBUG output 
