14e63aaee6fb9169d310509db70d01035a42f36d
Yuxi Liu <liu.yuxi.canaan@gmail.com>
2024-03-29 22:32:24 +0800

    查询相关的逻辑

--- test.sh
+++ test.sh
@@ -3,12 +3,14 @@ sudo ./firewall start
 sudo ./firewall clear
 sudo ./firewall default DROP --all
 #sudo ./firewall nat --snat --lan enp0s8 --wan enp0s3
-sudo ./firewall nat --snat --from-inter enp0s8 --to-inter enp0s3
-sudo ./firewall save
+sudo ./firewall nat --snat --from-inter enp0s8 --to-inter enp0s3 --log firewall-forward:
+sudo ./firewall save --log-input firewall-input-drop:
 
-sudo iptables -nvL
-sudo iptables -t nat -nvL
+sudo ./firewall filter --list
+# sudo ./firewall nat --list
+# sudo iptables -nvL
+# sudo iptables -t nat -nvL
 
 #TODO
 # log
-# DEBUG output 
+# DEBUG output
